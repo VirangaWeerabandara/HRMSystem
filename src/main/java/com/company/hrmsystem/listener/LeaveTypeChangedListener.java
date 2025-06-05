049ff2c9d41e18c42f415a1085f25b15c32aad5e
VirangaWeerabandara <umiduviranga8@gmail.com>
2025-06-05 14:08:23 +0530

    fix: Remove unused import in LeaveTypeChangedListener

--- src/main/java/com/company/hrmsystem/listener/LeaveTypeChangedListener.java
+++ src/main/java/com/company/hrmsystem/listener/LeaveTypeChangedListener.java
@@ -5,7 +5,6 @@ import com.company.hrmsystem.service.LeaveCalculationService;
 import io.jmix.core.event.EntityChangedEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
